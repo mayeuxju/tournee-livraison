85831276dda945c70b0983f9ed653d5bb5c571d7
mayeuxju <mayeuxju91@gmail.com>
2026-01-07 00:39:15 +0100

    Create secrets.py

---
+++ .streamlit/secrets.py
@@ -0,0 +1,8 @@
+# secrets.py
+
+# Remplacez ceci par votre vraie clé API Google Maps Platform
+# Assurez-vous que cette clé est configurée pour autoriser les appels aux APIs :
+# - Geocoding API
+# - Directions API
+# - Places API (optionnel, mais utile si vous récupérez des infos détaillées)
+GOOGLE_API_KEY = "AIzaSyA31LqMOI4B-99NCUSf1nPnpSwcWjqga4U"
